a394f542e7064d39c850b4e0fc680c6a7b150cbb
Ong Hin Journ <onghinjourn@gmail.com>
2023-12-26 04:29:26 +0800

    feat: :sparkles: adds missing sections to new home and moves it to index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import alpinejs from '@astrojs/alpinejs';
-import markdoc from '@astrojs/markdoc';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -8,5 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://notlega.github.io',
-  integrations: [alpinejs(), markdoc(), partytown(), sitemap(), tailwind()],
+  trailingSlash: 'never',
+  integrations: [alpinejs(), partytown(), sitemap(), tailwind()],
 });
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const WEBSITE_URL = 'https://notlega.github.io';
+export const GITHUB_URL = 'https://github.com/notlega';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/ong-hin-journ';
+export const GMAIL = 'onghinjourn@gmail.com';
