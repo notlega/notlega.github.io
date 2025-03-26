804205993885d7d2f84b3fb57f55d9a66c343e4d
Ong Hin Journ <onghinjourn@gmail.com>
2025-03-26 22:12:12 +0800

    feat: :sparkles: updates portfolio with new info

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,45 @@ export const WEBSITE_URL = 'https://notlega.github.io';
 export const GITHUB_URL = 'https://github.com/notlega';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/ong-hin-journ';
 export const GMAIL = 'onghinjourn@gmail.com';
+export const PROJECTS = [
+  {
+    title: 'Food Innovation and Resource Centre (FIRC) - Software Engineer',
+    date: 'April 2022 - August 2022',
+    description: [
+      'Used the MVC design pattern to design and code, which improves re-usability and maintainability.',
+      'Wrote logging for debugging and testing purposes for the entire project to improve productivity.',
+      'Wrote unit test cases to maintain the application and prevent bugs, reducing costs.',
+      'Used React, a popular Front-End framework, together with charting and styling libraries using best practices to improve reusability and maintainability.',
+      'Wrote end-to-end test cases to maintain application and eliminate bugs.',
+      'Participated in the setting up, maintaining and deployment of the PostgreSQL relational database for development and production.',
+    ],
+  },
+  {
+    title: 'Singapore Iron Works Merchant Association - DevOps Engineer',
+    date: 'September 2022 - September 2023',
+    description: [
+      'Used NextJS, a popular Front-End framework, together with popular styling libraries using best practices to improve responsiveness and maintainability.',
+      'Wrote end-to-end test cases to maintain the application and reduce bugs.',
+      'In V1 of the WebApp, I contributed to the design of the database.',
+      'In V2 of the WebApp, I participated in the migration and setup of the database from Supabase to CockroachDB and Prisma as our ORM.',
+      'Wrote testing and deployment pipelines using GitHub Actions.',
+      'This improved productivity by 100% as manual testing and deployment was not needed anymore.',
+      'Set-up AWS ECR, ECS, and S3 for preview, staging and production.',
+      'Wrote documentation for teams to help them set up a local testing environment using Docker and Bash/Powershell.',
+      "This reduced the team's time wasted searching for methods to set up their local testing environment.",
+    ],
+  },
+];
+export const WORK_EXPERIENCE = [
+  {
+    title: 'Intygrate Pte Ltd - Junior Developer',
+    date: 'July 2023 - Februrary 2024',
+    description: [
+      'Used HandlebarJS and ExpressJS, popular choices for Full-Stack, using best practices to improve reusability and maintainability.',
+      'Wrote end-to-end test cases to maintain the application and eliminate bugs.',
+      'Wrote seeding and deployment scripts for development and production of the MongoDB NoSQL database.',
+      'Wrote documentation for teams to help them set up a local testing environment.',
+      "This reduced the team's time wasted searching for methods to set up their local testing environment.",
+    ],
+  }
+]
